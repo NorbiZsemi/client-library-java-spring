c827b9126020a83dbcec8b764e4b02265d8efe6e
Norbert Weller <norbizsemi@gmail.com>
2021-03-10 16:45:47 +0100

    Fixing debug print

--- src/main/java/eu/arrowhead/client/library/ArrowheadService.java
+++ src/main/java/eu/arrowhead/client/library/ArrowheadService.java
@@ -324,7 +324,7 @@ public class ArrowheadService {
 	 * @throws UnavailableServerException when one of the core system involved in orchestration process is not available 
 	 */
 	public OrchestrationResponseDTO proceedOrchestration(final OrchestrationFormRequestDTO request) {
-		System.console().printf("Test debug print!");
+		System.out.println("Test debug print!");
 		final CoreServiceUri uri = getCoreServiceUri(CoreSystemService.ORCHESTRATION_SERVICE);
 		if (uri == null) {
 			logger.debug("Orchestration couldn't be proceeded due to the following reason: " +  CoreSystemService.ORCHESTRATION_SERVICE.name() + " not known by Arrowhead Context");
